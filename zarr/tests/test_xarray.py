2e073cb803370563244bad8582e73e009e3ba44d
Matthias Bussonnier <bussonniermatthias@gmail.com>
2020-08-06 07:53:03 -0700

    test only on 3.8

--- zarr/tests/test_xarray.py
+++ zarr/tests/test_xarray.py
@@ -6,29 +6,34 @@ from zarr import DirectoryStore
 
 from pathlib import Path
 
-
-def test_xarray():
-    p = Path('rasm.nc')
-    if not p.exists():
-        r = requests.get('https://github.com/pydata/xarray-data/raw/master/rasm.nc')
-        with open('rasm.nc', 'wb') as f:
-            f.write(r.content)
-
-    ds = xr.open_dataset('rasm.nc')
+import sys
 
 
-    compressor = zarr.Blosc(cname='zstd', clevel=3)
-    encoding = {vname: {'compressor': compressor} for vname in ds.data_vars}
+if sys.version_info >= (3 , 8):
+
+    def test_xarray():
+
+        p = Path('rasm.nc')
+        if not p.exists():
+            r = requests.get('https://github.com/pydata/xarray-data/raw/master/rasm.nc')
+            with open('rasm.nc', 'wb') as f:
+                f.write(r.content)
+
+        ds = xr.open_dataset('rasm.nc')
 
 
-    v33 = SyncV3DirectoryStore('v3.zarr')
-    v23 = V2from3Adapter(v33)
+        compressor = zarr.Blosc(cname='zstd', clevel=3)
+        encoding = {vname: {'compressor': compressor} for vname in ds.data_vars}
 
-    # use xarray to write to a v3 store via the adapter, so this will create a v3-zarr file
-    ds.to_zarr(v23, encoding=encoding)
 
-    # now we open directly the v3 store and check we get the right things
-    zarr_ds = xr.open_zarr(store=v33)
+        v33 = SyncV3DirectoryStore('v3.zarr')
+        v23 = V2from3Adapter(v33)
 
-    assert len(zarr_ds.attrs) == 11
-    assert zarr_ds.Tair.shape == (36, 205, 275)
+        # use xarray to write to a v3 store via the adapter, so this will create a v3-zarr file
+        ds.to_zarr(v23, encoding=encoding)
+
+        # now we open directly the v3 store and check we get the right things
+        zarr_ds = xr.open_zarr(store=v33)
+
+        assert len(zarr_ds.attrs) == 11
+        assert zarr_ds.Tair.shape == (36, 205, 275)
